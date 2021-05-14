ef91df6c65b2e1d0d27207ee0eb701031f2ef56d
Debra <dsparr1010@gmail.com>
2021-05-13 21:22:45 -0500

    make requests to githubs user repos and commits endpoints. connect postgres db and migrate.

--- developer_journal/settings.py
+++ developer_journal/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8cikzr4u#el1s3)*re1@-g*jiaqa8@!de)=$o&t8(4tqg7mssx'
+SECRET_KEY = os.getenv('SUPER_SECRET_APP_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ developer_journal/views/initial.py
@@ -0,0 +1,40 @@
+from django.http import HttpResponse, JsonResponse
+from django.views import View
+import requests
+import json
+import os
+
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
+    "Accept":"application/vnd.github.v3+json",
+    "Content-Type":"application/json; charset=utf-8",
+    "Authorization" : "Token "+os.getenv('GH_ACCESS_TOKEN')
+}
+
+class InitialView(View):
+    def get(self, request, *args, **kwargs):
+
+        # GET REPOS FOR AUTHENTICATED USER (ME)
+        # r_repos = requests.get('https://api.github.com/user/repos',params={'visibility':'all', 'sort':'created',
+        #                                                             'direction':'desc', 'per_page':5, 'page':1}, headers=HEADERS)
+
+        # GET OLLIE'S PUBLIC REPOS
+        r_repos = requests.get('https://api.github.com/users/obf73/repos',params={'visibility':'all', 'sort':'created',
+                                                                    'direction':'desc', 'per_page':5, 'page':1}, headers=HEADERS)
+        if r_repos.status_code != 200:
+            print(r_repos.status_code)
+            print(r_repos.text)
+        data = json.loads(r_repos.text)
+        content = []
+        for i in data:
+            r_commits = requests.get(f'https://api.github.com/repos/obf73/{i["name"]}/commits', params={'per_page':5, 'page':1},
+                                     headers=HEADERS)
+            if r_commits.status_code != 200:
+                print(r_commits.status_code)
+                print(r_commits.text)
+            data = json.loads(r_commits.text)
+            # print(json.dumps(data, indent=2))
+            content.append(data)
+        return JsonResponse(data=content, safe=False)
+
